3a0794b3e83bf865902007f9e1dfacd1761f67b0
leema-code <tesleema22@gamil.com>
2021-08-22 22:36:14 -0500

    back up of orig pom.xml from complete directory

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,13 +5,40 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// import the Prometheus packages.
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+// Prometheus counter package.
+import io.prometheus.client.Counter;
+// Prometheus Histogram package.
+import io.prometheus.client.Histogram;
+
 @SpringBootApplication
 @RestController
-public class Application {
+// Add a Prometheus metrics enpoint to the route `/prometheus`. `/metrics` is already taken by Actuator.
+@EnablePrometheusEndpoint
+// Pull all metrics from Actuator and expose them as Prometheus metrics. Need to disable security feature in properties file.
+@EnableSpringBootMetricsCollector
 
+public class Application {
+	// Define a counter metric for /prometheus
+	static final Counter requests = Counter.build()
+			.name("requests_total").help("Total number of requests.").register();
+	// Define a histogram metric for /prometheus
+	static final Histogram requestLatency = Histogram.build()
+			.name("requests_latency_seconds").help("Request latency in seconds.").register()
 	@RequestMapping("/")
 	public String home() {
+		// Increase the counter metric
+		requests.inc();
+		// Start the histogram timer
+		Histogram.Timer requestTimer = requestLatency.startTimer();
+		try {
 		return "This is a Monitoring Project";
+		} finally {
+			// Stop the histogram timer
+			requestTimer.observeDuration();
+		}
 	}
 
 	public static void main(String[] args) {
