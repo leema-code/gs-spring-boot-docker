3c4413fa4357aa22db2ed6f382f9445bf4b631b7
leema-code <tesleema22@gamil.com>
2021-08-22 21:42:00 -0500

    changed text

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "This is a Monitoring Project";
 	}
 
 	public static void main(String[] args) {
